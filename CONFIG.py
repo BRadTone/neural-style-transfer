76d6c7f15b7989a5223b2ab593749e14ac0ffb68
Radosław Białowąs <radoslaw.bialowas@gmail.com>
2018-06-04 09:52:52 +0200

    added get_pretrained_model script. Downloading vgg-16 model.

---
+++ CONFIG.py
@@ -0,0 +1,3 @@
+class CONFIG:
+    VGG_VERYDEEP_16_DOWNLOAD_URL = 'http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-16.mat'
+    PRETRAINED_MODEL_PATH = './pretrained_model/imagenet-vgg-verydeep-16.mat'
